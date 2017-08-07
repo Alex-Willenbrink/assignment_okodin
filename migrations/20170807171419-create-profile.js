4c147e12d1229eded3c41c0e98c74a14f572f2ee
Greg Filipczak <gregfilipczak@gmail.com>
2017-08-07 17:58:45 +0000

    seeds

--- migrations/20170807171419-create-profile.js
+++ migrations/20170807171419-create-profile.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('Profiles', {
+    return queryInterface.createTable("Profile", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -47,16 +47,16 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.fn('NOW')
+        defaultValue: Sequelize.fn("NOW")
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.fn('NOW')
+        defaultValue: Sequelize.fn("NOW")
       }
     });
   },
   down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Profiles');
+    return queryInterface.dropTable("Profile");
   }
 };
